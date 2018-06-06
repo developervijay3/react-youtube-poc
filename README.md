0bc1f723d229085727d053ce486f95fa19b9a8eb
Suraj Kumar <developervijay3@gmail.com>
2018-06-06 15:30:09 -0400

    files for React

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# react-youtube-poc
-Uploading files for React App
+# Simple Youtube App created
+
+
+### Getting Started
+
+There are two methods for getting started with this repo.
+
+#### Familiar with Git?
+Checkout this repo, install dependencies, then start the gulp process with the following:
+
+```
+> git clone https://github.com/sharvaibhav/react-youtube-poc.git
+> cd react-youtube-poc
+> npm install
+> npm start
+```
+
+## Enjoy!
---
+++ src/index.js
@@ -0,0 +1,48 @@
+// Create a new  component  this component should produce some html
+import ReactDOM from 'react-dom';
+import React,{Component} from 'react';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/videolist';
+import SearchBar from './components/searchbar';
+import VideoDetail from './components/videodetail';
+import _ from 'lodash';
+
+const API_KEY = 'AIzaSyDSAoFarYZUcm6ydoadIYBCpbctW3DFOCM';
+                    
+class App extends Component{
+  
+  constructor(props){
+    super(props);
+    this.state={
+      videos:[],
+      selectedVideo:null
+    };
+   this.videoSearch('surfboards');
+  }
+
+  videoSearch(term){
+    YTSearch({key:API_KEY,term:term},(videos) =>{
+      this.setState({
+          videos:videos,
+          selectedVideo:videos[0]  
+        });
+    });
+  }
+
+  render(){
+
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)},300);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList videos={this.state.videos} onVideoSelect={selectedVideo => this.setState({selectedVideo:selectedVideo}) } />        
+      </div>
+    );
+  }
+}
+  
+
+
+ReactDOM.render(<App />,document.getElementById('container'));
